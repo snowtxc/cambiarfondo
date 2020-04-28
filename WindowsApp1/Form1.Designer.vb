77270f09582edcd3206a86151f56caf261ec49c1
snowtxc <53797234+snowtxc@users.noreply.github.com>
2020-04-28 14:27:35 -0300

    cambiarfondosegunnumero

---
+++ WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.numText = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(136, 68)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Aceptar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'numText
+        '
+        Me.numText.Location = New System.Drawing.Point(136, 27)
+        Me.numText.Name = "numText"
+        Me.numText.Size = New System.Drawing.Size(51, 20)
+        Me.numText.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(35, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(95, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Ingresa un numero"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(467, 315)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.numText)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents numText As TextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim num As String
+
+        num = numText.Text
+
+        Select Case num
+            Case 1
+                Me.BackColor = Color.Black
+
+            Case 2
+                Me.BackColor = Color.Yellow
+
+            Case 3
+                Me.BackColor = Color.Blue
+
+            Case 4
+                Me.BackColor = Color.Red
+
+
+            Case 5
+                Me.BackColor = Color.Green
+
+            Case Else
+
+                Me.BackColor = Color.White
+
+        End Select
+
+
+
+    End Sub
+End Class
